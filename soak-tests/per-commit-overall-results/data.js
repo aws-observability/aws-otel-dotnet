162e55b9a6e3a0c05b0f1814b43810c527aa6c19
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 20:06:26 +0000

    add Soak Test Results - sample-app-microsoft_dotnet-manual (custombenchmark) benchmark result for 6d25cdad2a24e153021cfa43284ea9d585ba182c

---
+++ soak-tests/per-commit-overall-results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632427586729,
+  "repoUrl": "https://github.com/aws-observability/aws-otel-dotnet",
+  "entries": {
+    "Soak Test Results - sample-app-microsoft_dotnet-manual": [
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "6d25cdad2a24e153021cfa43284ea9d585ba182c",
+          "message": "Add Soak Tests (#7)\n\n* Add Soak Tests Files Unmodified from ADOT Python\r\n\r\n* Modify the Soak Tests for ADOT .NET Downstream",
+          "timestamp": "2021-09-22T20:48:39Z",
+          "url": "https://github.com/aws-observability/aws-otel-dotnet/commit/6d25cdad2a24e153021cfa43284ea9d585ba182c"
+        },
+        "date": 1632427586340,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 52.13324404761905,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 13676.699488146553,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
